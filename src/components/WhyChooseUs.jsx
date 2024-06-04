65a646ab1da8440823e460e133cfdf0d6ce5a886
Matt <matt.atkins216@gmail.com>
2024-06-04 17:09:05 -0600

    making fun edits to get it better looking

--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -3,32 +3,32 @@ import React from 'react'
 const WhyChooseUs = () => {
   return (
     <>
-    
-     <div className='flex flex-row self-center pt-10 w-screen justify-center bg-lightpink'>
+
+      <div className='flex flex-row self-center pt-10 w-screen justify-center bg-lightpink'>
         <h4 className='text-3xl font-medium font-poppins self-center'>Why Choose Us? </h4>
-     </div>
-
-    <div className='flex flex-col md:flex-row h-auto font-poppins items-center py-10 gap-10 self-center bg-lightpink w-screen justify-center'>
-      <div className='flex flex-col gap-2 bg-white mx-4 p-4 md:w-96 drop-shadow-2xl rounded-xl md:gap-4 '>
-        <h6 className='text-xl font-medium text-center underline transition-transform '>We Come To You!</h6>
-        <p className='text-lg text-center mx-auto px-10'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus totam sed aliquam aspernatur odit ullam suscipit atque ipsam. Id dolor dicta doloribus deleniti earum? Molestias omnis sint nihil quam architecto?</p>
-
       </div>
 
-      <div className='flex flex-col gap-2 bg-white mx-4 p-4 md:w-96 drop-shadow-2xl  rounded-xl md:gap-4 '>
-        <h6 className='text-xl font-medium text-center underline transition-transform '>We Come To You!</h6>
-        <p className='text-lg text-center mx-auto px-10'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus totam sed aliquam aspernatur odit ullam suscipit atque ipsam. Id dolor dicta doloribus deleniti earum? Molestias omnis sint nihil quam architecto?</p>
+      <div className='flex flex-col md:flex-row h-auto font-poppins items-center py-10 gap-10 self-center bg-lightpink w-screen justify-center'>
+        <div className='flex flex-col gap-2 bg-white mx-4 p-4 md:w-96 drop-shadow-2xl rounded-xl md:gap-4 '>
+          <h6 className='text-xl font-medium text-center underline transition-transform '>We Come To You!</h6>
+          <p className='text-lg text-center mx-auto px-10'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus totam sed aliquam aspernatur odit ullam suscipit atque ipsam. Id dolor dicta doloribus deleniti earum? Molestias omnis sint nihil quam architecto?</p>
+
+        </div>
+
+        <div className='flex flex-col gap-2 bg-white mx-4 p-4 md:w-96 drop-shadow-2xl  rounded-xl md:gap-4 '>
+          <h6 className='text-xl font-medium text-center underline transition-transform '>We Come To You!</h6>
+          <p className='text-lg text-center mx-auto px-10'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus totam sed aliquam aspernatur odit ullam suscipit atque ipsam. Id dolor dicta doloribus deleniti earum? Molestias omnis sint nihil quam architecto?</p>
+
+        </div>
+
+        <div className='flex flex-col gap-2 bg-white mx-4 p-4 md:w-96 drop-shadow-2xl  rounded-xl md:gap-4 '>
+          <h6 className='text-xl font-medium text-center underline transition-transform '>We Come To You!</h6>
+          <p className='text-lg text-center mx-auto px-10'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus totam sed aliquam aspernatur odit ullam suscipit atque ipsam. Id dolor dicta doloribus deleniti earum? Molestias omnis sint nihil quam architecto?</p>
+
+        </div>
+
 
       </div>
-
-      <div className='flex flex-col gap-2 bg-white mx-4 p-4 md:w-96 drop-shadow-2xl  rounded-xl md:gap-4 '>
-        <h6 className='text-xl font-medium text-center underline transition-transform '>We Come To You!</h6>
-        <p className='text-lg text-center mx-auto px-10'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Temporibus totam sed aliquam aspernatur odit ullam suscipit atque ipsam. Id dolor dicta doloribus deleniti earum? Molestias omnis sint nihil quam architecto?</p>
-
-      </div>
-      
-    
-    </div>
     </>
   )
 }
